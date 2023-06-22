f13d7b01fbfc63edc41e1d20ea0b943dffef9fc1
kimyegun123 <kimyegun123@gmail.com>
2023-06-22 15:37:40 +0900

    Spring-177

---
+++ ex02_p177/src/main/java/org/zerock/mapper/TimeMapper.java
@@ -0,0 +1,11 @@
+package org.zerock.mapper;
+
+import org.apache.ibatis.annotations.Select;
+
+public interface TimeMapper {
+		
+	@Select("SELECT sysdate FROM dual")
+	public String getTime();
+	
+	public String getTime2();
+}
